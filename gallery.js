7dee9c0dc8da6befada0a6f9d23cc57dadcf898e
User319183 <User319183@users.noreply.github.com>
2025-11-16 20:50:51 -0500

    Add timer function for auto-advancing slides

--- gallery.js
+++ gallery.js
@@ -7,6 +7,7 @@ $(document).ready(() => {
   $('.details').hide() // Hide details initially
 
   // Call a function here to start the timer for the slideshow
+  startTimer()
 
   // Select the moreIndicator button and add a click event to:
   // - toggle the rotation classes (rot90 and rot270)
@@ -81,6 +82,5 @@ function showPrevPhoto () {
 // Starter code for the timer function
 function startTimer () {
   // Create a timer to automatically call `showNextPhoto()` every mWaitTime milliseconds
-  // Consider using setInterval to achieve this functionality
-  // Hint: Make sure only one timer runs at a time
+  setInterval(showNextPhoto, mWaitTime)
 }
